158448bf3d87a05508df300c3124beab428eed36
elmeradrianv <elmeradrianv@outlook.com>
2023-08-30 21:03:04 -0600

    First part of the entities database

--- text.py
+++ text.py
@@ -8,3 +8,29 @@ script = open(script_path + script_file, 'r')
 script_text = script.read()
 script.close()
 # %%
+from google.cloud import language_v1
+
+
+
+def sample_analyze_entities():
+    # Create a client
+    client = language_v1.LanguageServiceClient()
+
+    # Initialize request argument(s)
+    document = language_v1.Document()
+    script_file = '3 Idiots_1187043_lemmas.txt'
+    script = open(script_path + script_file, 'r')
+    script_text = script.read()
+    document.content = script_text
+
+    request = language_v1.AnalyzeEntitiesRequest(
+        document=document,
+    )
+
+    # Make the request
+    response = client.analyze_entities(request=request)
+
+    # Handle the response
+    print(response)
+
+sample_analyze_entities()
